796620878f299d1ee5f609eba2263a08edd18ae6
Kevin <kevin.norris.90@gmail.com>
2017-01-10 01:33:26 +0000

    implemented header parsing

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #Second Backend API project for freeCodeCamp
 
-Extracts the users ip adress, language and operating system from the request to the server and returns them in a JSON object
+Extracts the users ip adress, language and operating system from the request to the server and returns them in a JSON object.
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+
+app.get("/", function(req, res){
+    console.log(JSON.stringify(req.headers));
+    var ip = req.headers['x-forwarded-for'],
+        lang = req.headers["accept-language"],
+        userAgent = req.headers["user-agent"];
+        
+    res.set("Content-Type", "application/json");
+    res.send({
+        "ipaddress": ip,
+        "language": lang.indexOf(",") ? lang.substring(0,lang.indexOf(",")) : lang,
+        "os": userAgent.substring(userAgent.indexOf("(") + 1, userAgent.indexOf(")")),
+        "browser": userAgent.substr(0, userAgent.indexOf(" "))
+    });
+});
+
+app.listen(process.env.PORT || 8080);
